611f583dd224c49ef85ca9cb343015b420b4f505
Nikhil Yadav <nkrider27@gmail.com>
2021-05-11 15:53:44 +0530

    Update model.py

--- model.py
+++ model.py
@@ -30,23 +30,32 @@ model = Sequential([
     MaxPooling2D(pool_size=(1,1)),
     Conv2D(32,(3,3),activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
+    
 #32 convolution filters used each of size 3x3
 #again
+    
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(pool_size=(1,1)),
 
 #64 convolution filters used each of size 3x3
-#choose the best features via pooling
     
+#choose the best features via pooling
+   
 #randomly turn neurons on and off to improve convergence
+    
     Dropout(0.25),
 #flatten since too many dimensions, we only want a classification output
+    
     Flatten(),
+    
 #fully connected to get all relevant data
+    
     Dense(128, activation='relu'),
 #one more dropout for convergence' sake :) 
+    
     Dropout(0.5),
 #output a softmax to squash the matrix into output probabilities
+    
     Dense(2, activation='softmax')
 ])
 
